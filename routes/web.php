9b3c9b66312cafdeb33cf5e7f2cdb004e5bc16dc
3A417118 <s3A417118@student.ncut.edu.tw>
2018-10-30 20:54:31 +0800

    Route Settings

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,15 @@ Route::get('hello/{name?}',function($name='Everybody'){
 Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return view('welcome');
 }]);
+
+
+/* 設定 dashboard路徑的 Route */
+ Route::get('dashboard',function(){
+    return 'dashboard';
+});
+/*設定Route 以群組包起來設定 prefix */
+ Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return 'admindashboard';
+    });
+});
